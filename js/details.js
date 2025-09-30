c553e480e3d31fbbdfb52ad3fdcbe1e319127543
Abdullah Salah <91805490+ALbody@users.noreply.github.com>
2025-09-30 23:33:24 +0300

    Add files via upload

---
+++ js/details.js
@@ -0,0 +1,23 @@
+const buttons = document.querySelectorAll(".whatsapp-btn");
+
+buttons.forEach(btn => {
+  btn.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    // البطاقة الأقرب
+    const card = this.closest(".box-content.project-detail");
+    const title = card.querySelector("h2").innerText.trim();
+
+    // رقم الواتساب (بدون + أو مسافات)
+    const phone = "201019890771";
+
+    // الرسالة
+    var message = "مرحبا، أريد تفاصيل عن المنتج: " + title;
+
+    // رابط الواتساب
+    var url = "https://wa.me/" + phone + "?text=" + encodeURIComponent(message);
+
+    // فتح الرابط
+    window.open(url, "_blank");
+  });
+});
